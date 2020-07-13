d12259f927e79bf2af79255d73f4c5f083fd4540
Saumya <saumya.1126@gmail.com>
2020-07-13 19:23:34 +0530

    Display message and corresponding table

--- RDIP/Codes/unit4.js
+++ RDIP/Codes/unit4.js
@@ -4,3 +4,21 @@ corpus2=["A wolf carried off a lamb. The lamb said, 'I know you are going to eat
 
 corpus3=["A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. 'Why does he not make a pet of me?' said the donkey. 'It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master\'s knee. It is not fair.' Then the donkey said to himself, 'If I do what the dog does, he may make a pet of me.' So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master\'s knee. The master thought the donkey was mad, and he shouted, 'Help! Help!' Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field. 'I only did what the dog does,' said the donkey,' and yet they make a pet of the dog, and they beat me with sticks. It is not fair."]
 
+function showCorpus(){
+	var x=document.getElementById("selected").value;
+	if(x=='Corpus1'){
+		document.getElementById("corp1").innerHTML=corpus1;
+		document.getElementById("display").innerHTML="Enter the correct number of tokens and types from the corpus:";
+		document.getElementById("tab").innerHTML="<table><tr><td>"+"#tokens"+"</td><td>"+"<input type='text'>"+"</td></tr><tr><td>"+"#types"+"</td><td>"+"<input type='text'>"+"</td></tr></table>";
+	}
+	if(x=='Corpus2'){
+		document.getElementById("corp1").innerHTML=corpus2;
+		document.getElementById("display").innerHTML="Enter the correct number of tokens and types from the corpus:";
+		document.getElementById("tab").innerHTML="<table><tr><td>"+"#tokens"+"</td><td>"+"<input type='text'>"+"</td></tr><tr><td>"+"#types"+"</td><td>"+"<input type='text'>"+"</td></tr></table?";
+	}
+	if(x=='Corpus3'){
+		document.getElementById("corp1").innerHTML=corpus3;
+		document.getElementById("display").innerHTML="Enter the correct number of tokens and types from the corpus:";
+		document.getElementById("tab").innerHTML="<table><tr><td>"+"#tokens"+"</td><td>"+"<input type='text'>"+"</td></tr><tr><td>"+"#types"+"</td><td>"+"<input type='text'>"+"</td></tr></table>";
+	}
+}
