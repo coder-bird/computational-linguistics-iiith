a97bc62b1b772d5daaff054daf8e8597d7667551
Saumya <saumya.1126@gmail.com>
2020-07-17 15:58:17 +0530

    Making language and sentence selection dropdown

--- RDIP_2/Codes/unit5.js
+++ RDIP_2/Codes/unit5.js
@@ -10,7 +10,7 @@ SH3=["मेहनत", "का", "फल", "मीठा", "होता", "ह
 SH4=["वाह!", "वह", "खूबसूरत", "है।"]
 SH5=["पेड़", "से", "पत्ते", "गिर", "गए।"]
 
-document.getElementById('1').innerHTML= SE1.join(' ');
+/*document.getElementById('1').innerHTML= SE1.join(' ');
 document.getElementById('2').innerHTML= SE2.join(' ');
 document.getElementById('3').innerHTML= SE3.join(' ');
 document.getElementById('4').innerHTML= SE4.join(' ');
@@ -19,4 +19,14 @@ document.getElementById('6').innerHTML= SH1.join(' ');
 document.getElementById('7').innerHTML= SH2.join(' ');
 document.getElementById('8').innerHTML= SH3.join(' ');
 document.getElementById('9').innerHTML= SH4.join(' ');
-document.getElementById('10').innerHTML= SH5.join(' ');
+document.getElementById('10').innerHTML= SH5.join(' '); */
+function chooseLang(){
+	var optn= document.getElementById('selected').value;
+	if(optn=='english'){
+		document.getElementById('senDropbox').innerHTML="<select><option value='null'>"+ "---Select a sentence---" +"</option>"+"<option value='1'>"+ SE1.join(" ") +"</option>"+"</option>"+"<option value='2'>"+ SE2.join(" ") +"</option>"+"</option>"+"<option value='3'>"+ SE3.join(" ") +"</option>"+"</option>"+"<option value='4'>"+ SE4.join(" ") +"</option>"+"</option>"+"<option value='5'>"+ SE5.join(" ") +"</option></select>"
+	}
+	if(optn=='hindi'){
+		document.getElementById('senDropbox').innerHTML="<select><option value='null'>"+ "---Select a sentence---" +"</option>"+"<option value='6'>"+ SH1.join(" ") +"</option>"+"</option>"+"<option value='7'>"+ SH2.join(" ") +"</option>"+"</option>"+"<option value='8'>"+ SH3.join(" ") +"</option>"+"</option>"+"<option value='9'>"+ SH4.join(" ") +"</option>"+"</option>"+"<option value='10'>"+ SH5.join(" ") +"</option></select>"
+	}
+	
+}
